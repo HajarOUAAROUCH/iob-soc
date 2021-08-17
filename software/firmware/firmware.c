0bc6e46a1f42a519039a125eeb37acc3e91c3274
HajarOUAAROUCH <Hajar.ouaarouch.1212@gmail.com>
2021-08-17 14:58:01 +0100

    commit6

--- software/firmware/firmware.c
+++ software/firmware/firmware.c
@@ -1,15 +1,26 @@
-#include "stdlib.h"
-#include <stdio.h>
 #include "system.h"
 #include "periphs.h"
 #include "iob-uart.h"
-#include "printf.h"
+#include "iob-timer.h"
 
 int main()
 {
-  //init uart
-  uart_init(UART_BASE,FREQ/BAUD);   
-  uart_puts("\n\n\nHello world!\n\n\n");
-  printf("Value of Pi = %f\n\n", 3.1415);
-  uart_finish();
+  unsigned long long elapsed;
+  unsigned int elapsedu;
+
+  //Read current Timer count, compute elapsed
+  elapsed = timer_get_count(TIMER_BASE);
+  elapsedu = timer_time_us(TIMER_BASE);
+
+  //init UART
+  uart_init(UART_BASE,FREQ/BAUD);
+  
+  uart_printf("\n\n\nHello world!\n\n\n");
+
+  uart_txwait ();
+  
+  uart_printf("\nExecution time: %d clocks in %dus @%dMHz (%d MBaud)\n\n",(unsigned int)elapsed, elapsedu, FREQ/1000000, BAUD/1000000);
+
+  uart_txwait ();
+  return 0;
 }
